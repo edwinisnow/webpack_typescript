fcf2c5f023f870d42afa1f355dbf6ee74c62527a
EDWIN GEORGE LAZER <edwin04@gmail.com>
2023-09-16 06:38:22 +0100

    webpack extensions

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log('hello 1');
-console.log('hello 2');
+import { formData } from './forms';
+
+const form = document.querySelector('form')!
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const data = formData(form);
+    console.log(data);
+});
